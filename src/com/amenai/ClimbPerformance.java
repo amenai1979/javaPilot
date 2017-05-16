d049550f374f3c6c568f4272eb0e90f9d562139f
Menai, Alex <amenai@akamai.com>
2017-05-16 08:19:10 -0400

    adding methods and instance variables for CommonPerformance Elements and ClimbPerformance

--- src/com/amenai/ClimbPerformance.java
+++ src/com/amenai/ClimbPerformance.java
@@ -4,4 +4,21 @@ package com.amenai;
  * Created by amenai on 5/14/17.
  */
 public class ClimbPerformance {
+    private int climbPerformanceFPM;
+    void setClimbPerformanceFPM(int fpm){
+
+    }
+    int getClimbPerformanceFPM(){
+        return climbPerformanceFPM;
+    }
+    double getClimbFuelGallons(){
+        return 0.0;
+    }
+    double getClimbTimeMinutes()
+    {
+        return 0.0;
+    }
+    double getClimbDistanceNM(){
+        return 0.0;
+    }
 }
--- src/com/amenai/CommonPerformanceElement.java
+++ src/com/amenai/CommonPerformanceElement.java
@@ -4,4 +4,50 @@ package com.amenai;
  * Created by amenai on 5/14/17.
  */
 public class CommonPerformanceElement {
+    private int OATC;
+    private int weightLb;
+    private boolean gearDown;
+    private int flapsSetting;
+    private int tasKts;
+    private int altitudeFt;
+    private int axisWindKts;
+    void setOATC(int oat){
+    }
+    int getOATC(){
+        return OATC;
+    }
+    void setWeightLb(int weight){
+    }
+    int getWeightLb(){
+        return weightLb;
+    }
+    void setGearDown(boolean gear){
+    }
+    boolean getGearDown(){
+        return gearDown;
+    }
+    void setFlapsSetting(int flaps){
+
+    }
+    int getFlapsSetting(){
+        return flapsSetting;
+    }
+    void setTasKts(int ias){
+
+    }
+    int getTasKts(){
+        return tasKts;
+    }
+    void setAltitudeFt(int alt){
+
+    }
+    int getAltitudeFt(){
+        return altitudeFt;
+    }
+    void setAxisWindKts(int wind){
+
+    }
+    int getAxisWindKts(){
+        return axisWindKts;
+    }
 }
