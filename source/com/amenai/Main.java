67d62fb21b4626159496c2355aa375ad2393dc13
root <root@vps50473.ovh.net>
2017-07-09 14:28:23 +0200

    adding classes and jars

--- source/com/amenai/Main.java
+++ source/com/amenai/Main.java
@@ -1,8 +1,9 @@
 package com.amenai;
-
+//import System.out.println;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	System.out.println("Hello World!");
     }
 }
