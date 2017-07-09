7c89930d5967a87b9b971fe084b6a78adb374117
root <root@vps50473.ovh.net>
2017-07-09 13:29:53 +0200

    reorganizing

---
+++ source/com/amenai/ClimbPerformance.java
@@ -0,0 +1,24 @@
+package com.amenai;
+
+/**
+ * Created by amenai on 5/14/17.
+ */
+public class ClimbPerformance extends CommonPerformanceElement {
+    private int climbPerformanceFPM;
+    void setClimbPerformanceFPM(int fpm){
+
+    }
+    int getClimbPerformanceFPM(){
+        return climbPerformanceFPM;
+    }
+    double getClimbFuelGallons(){
+        return 0.0;
+    }
+    double getClimbTimeMinutes()
+    {
+        return 0.0;
+    }
+    double getClimbDistanceNM(){
+        return 0.0;
+    }
+}
